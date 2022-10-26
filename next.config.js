f8df4ed4a6d1f085bbdd982e48940bb6a79a2e04
Punith <punith@pop-os.localdomain>
2022-10-26 18:36:26 +0530

    postcss config

--- next.config.js
+++ next.config.js
@@ -5,11 +5,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+const withCss = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
 
+module.exports = withCss(withPurgeCss({
+  purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer, // Only enable PurgeCSS for client-side production builds
 
-
-
-
+}
+));
 module.exports = withBundleAnalyzer({})
 
 module.exports = nextConfig
